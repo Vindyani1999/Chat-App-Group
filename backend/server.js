f9d7d15c10be214c038c0b973e5835d86c7b22b0
vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-05-12 03:59:39 +0530

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+// Package import
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+
+// Routes
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+// Database Part
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+
+// Variables
+const PORT = process.env.PORT || 5000;
+
+// .env config
+dotenv.config();
+
+
+//app.get("/",(req,res)=>{
+    // root route http://localhost:8000/
+    //res.send("Hello chamindu");
+//});
+
+
+// Midlware
+app.use(express.json()); // to parse the incoming requests with JSON payload (from req.body)
+app.use(cookieParser());
+
+// Routes Paths
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
+
+
+
+
+server.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server Running on port ${PORT}`)
+});
+
+
+
